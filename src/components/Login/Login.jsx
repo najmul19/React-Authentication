9094a3333998cc674d2a723f7d2036ac75536b10
najmul <mdnajmulislam10992@gmail.com>
2025-04-14 17:52:43 +0600

    handle condition sign up and sign out btn dynamically

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -34,8 +34,11 @@ const Login = () => {
     }
     return (
         <div>
-            <button onClick={handleGoogleSignIn}>Login with Google</button>
-            <button onClick={handleSignOut}>Sign Out</button>
+            
+            {
+                user ? <button onClick={handleSignOut}>Sign Out</button>:
+                <button onClick={handleGoogleSignIn}>Login with Google</button>
+            }
             {
                 user && <div>
                     
